7809533e1f9c676890c7c316e10e808f854dd47f
Alan <alanakra@hotmail.fr>
2021-11-22 19:26:08 +0100

    Remove useless script

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,5 @@
 import '../styles/main.scss'
 
-console.log('🎉')
-// Create a class property without a constructor
-class Game {  name = 'Violin Charades'}
-const myGame = new Game()
-// Create paragraph node
-const p = document.createElement('p')
-p.textContent = `I like ${myGame.name}.`
-
 // Create heading node
 const heading = document.createElement('h1')
 heading.textContent = 'Hello'
