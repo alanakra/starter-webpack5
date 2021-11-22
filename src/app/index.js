4ae6be59b7c70986deef1f20b42275c6dbc5352a
Alan <alanakra@hotmail.fr>
2021-11-22 18:13:27 +0100

    HTML template

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,9 @@
 console.log('🎉')
+
+// Create heading node
+const heading = document.createElement('h1')
+heading.textContent = 'Hello'
+
+// Append heading node to the DOM
+const app = document.querySelector('#root')
+app.append(heading)
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: {
     main: path.resolve(__dirname, './src/app/index.js'),
   },
-  
+
   output: {
    path: path.resolve(__dirname, './dist'),
    filename: '[name].bundle.js',
  },
+
+ plugins: [    
+    new HtmlWebpackPlugin({      
+      title: 'Webpack Starter',      
+      template: path.resolve(__dirname, './src/public/template.html'), // template file      
+      filename: 'index.html', // output file    
+     }),  
+ ],
 }
