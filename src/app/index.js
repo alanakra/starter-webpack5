255c5731c26022aadb6b5729b9cd35695dfd1a53
Alan <alanakra@hotmail.fr>
2021-11-23 19:52:51 +0100

    Remove uselss script

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,3 @@
 import '../styles/main.scss'
 
-// Create heading node
-const heading = document.createElement('h1')
-heading.textContent = 'Hello'
-
-// Append heading node to the DOM
-const app = document.querySelector('#root')
-app.append(heading)
+console.log('Imagine')
